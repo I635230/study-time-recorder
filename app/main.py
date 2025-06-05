dadb2a8a6f4a8ba12b21ee98ecc0028f27ee3acb
I635230 <yugo.susuki@gmail.com>
2025-06-06 01:13:13 +0900

    これでうまくいくかも

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 import asyncio
 import uvicorn
 from fastapi import FastAPI
-import threading
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -25,17 +24,14 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 app = FastAPI()
 
 # FastAPI
-# async def start_fastapi():
-#     config = uvicorn.Config(app=app, host="0.0.0.0", port=8000)
-#     server = uvicorn.Server(config)
-#     await server.serve()
-
-def run_fastapi():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+async def start_fastapi():
+    config = uvicorn.Config(app=app, host="0.0.0.0", port=8000)
+    server = uvicorn.Server(config)
+    await server.serve()
 
 @app.get("/")
-async def root():
-    return {"message": "Server is Online."}
+def read_root():
+    return {"status": "ok"}
 
 # 関数の登録
 def handle_vc_join(member, before, after):
@@ -141,23 +137,16 @@ async def daily_report_task():
     voice_durations.clear()
 
 # main
-# async def main():
-#     load_dotenv()
+async def main():
+    load_dotenv()
     
-#     thread = threading.Thread(target=run_fastapi, daemon=True)
-#     thread.start()
+    try:
+        await asyncio.gather(
+            start_fastapi(),
+            bot.start(os.getenv("DISCORD_TOKEN"))
+        )
+    except Exception as e:
+        print(f"起動中にエラーが発生しました: {e}")
 
-#     try:
-#         await bot.start(os.getenv("DISCORD_TOKEN"))
-#     except Exception as e:
-#         print(f"起動中にエラーが発生しました: {e}")
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
-
-load_dotenv()
-
-thread = threading.Thread(target=run_fastapi, daemon=True)
-thread.start()
-
-bot.run(os.getenv("DISCORD_TOKEN"))
+if __name__ == "__main__":
+    asyncio.run(main())
