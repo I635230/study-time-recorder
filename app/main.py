f2f7dd43bb55df4fe89af5943c966e281213feb6
I635230 <yugo.susuki@gmail.com>
2025-06-05 23:23:26 +0900

    test

--- app/main.py
+++ app/main.py
@@ -137,16 +137,17 @@ async def daily_report_task():
     voice_durations.clear()
 
 # main
-async def main():
-    load_dotenv()
+# async def main():
+#     load_dotenv()
 
-    try:
-        await asyncio.gather(
-            # start_fastapi(),
-            bot.start(os.getenv("DISCORD_TOKEN"))
-        )
-    except Exception as e:
-        print(f"起動中にエラーが発生しました: {e}")
+#     try:
+#         await asyncio.gather(
+#             # start_fastapi(),
+#             bot.start(os.getenv("DISCORD_TOKEN"))
+#         )
+#     except Exception as e:
+#         print(f"起動中にエラーが発生しました: {e}")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    # asyncio.run(main())
+    bot.start(os.getenv("DISCORD_TOKEN"))
